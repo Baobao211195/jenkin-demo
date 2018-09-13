e33f7db2ef17c11f91fe387744c845c56356c5de
phamvanoanh <oanhmrphamvan@gmail.com>
2018-09-13 11:40:09 +0700

    init project

---
+++ src/main/java/edu/actuator/SpringbootActuator.java
@@ -0,0 +1,12 @@
+package edu.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuator {
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringbootActuator.class, args);
+	}
+}
