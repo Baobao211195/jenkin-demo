1310648c33e34d6dbc132431d75bd8a2621a2f55
phamvanoanh <oanhmrphamvan@gmail.com>
2018-09-29 16:21:25 +0700

    add atmq

--- src/main/java/edu/actuator/controller/ActuatorController.java
+++ src/main/java/edu/actuator/controller/ActuatorController.java
@@ -22,9 +22,6 @@ public class ActuatorController {
 		System.err.println("tesst");
 		System.err.println("tesst");
 		System.err.println("tesst");
-		//fdfdf
-		
-		//test
 
 		//branch 'master' of https://github.com/phamvanoanh/jenkin-demo.git
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
